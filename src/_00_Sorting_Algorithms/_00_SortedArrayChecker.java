6eda9d974cfbc1be499e5967ea65906736653c94
U have'nt <whaaqaaaidchbfgvruihgiudfgiufhjifo@gmail.com>
2019-01-05 17:24:14 -0800

    code

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -6,14 +6,30 @@ public class _00_SortedArrayChecker {
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
-
+		static boolean intArraySorted(int [] array1) {
+		for (int i = 0; i < array1.length - 1; i++) {
+			if(array1[i] > array1[i+1]) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
 	
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
-
+		static boolean doubleArraySorted(double [] array1) {
+			for (int i = 0; i < array1.length - 1; i++) {
+				if(array1[i] > array1[i+1]) {
+					return false;
+				}
+			}
+			return true;
+			
+		}
 	
 	//3. Write a static method called charArraySorted. 
 	//   This method takes in an array of characters
@@ -22,6 +38,14 @@ public class _00_SortedArrayChecker {
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
 
+		static boolean charArraySorted(char [] array1) {
+			for (int i = 0; i < array1.length - 1; i++) {
+				if(array1[i] > array1[i+1]) {
+					return false;
+				}
+			}
+			return true;
+		}
 	
 	//4. Write a static method called stringArraySorted. 
 	//   This method takes in an array of Strings
@@ -29,5 +53,14 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the String
 	//   array is in alphabetical order and false otherwise
 	//   (Use the compareTo(String) method)
+		static boolean stringArraySorted(String [] array1) {
+			for (int i = 0; i < array1.length -1; i++) {
+				if(array1[i].compareTo(array1[i + 1]) > 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+
 
 }
--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -24,20 +24,40 @@ class _01_SwappingDigits {
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+		int first = arr[0];
+		int second = arr[1];
+		arr[1] = first;
+		arr[0] = second;
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
-		
+		for (int i = 0; i < arr.length -1; i++) {
+			if(arr[i] > arr[i+1]) {
+				int first = arr[i];
+				int second = arr[i+1];
+				arr[i+1] = first;
+				arr[i] = second;
+			}
+		}
 	}
 	
 	//3. Complete the method so that it finds the middle number in the array.
 	//   *Hint* it helps to sort it first.
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		for (int i = 0; i < arr.length -1; i++) {
+			if(arr[i] > arr[i+1]) {
+				int first = arr[i];
+				int second = arr[i+1];
+				arr[i+1] = first;
+				arr[i] = second;
+			}
+		}
+		int z = arr.length+1;
+		int i = z/2;
+		return arr[i];
 	}
 }
