89e4f2a1b46a32b107b166a7b6995d423973a11b
An Idiot <iman@idi.ot>
2019-01-26 17:24:10 -0800

    Working on quick sort

--- src/_00_Sorting_Algorithms/SelectionSorter.java
+++ src/_00_Sorting_Algorithms/SelectionSorter.java
@@ -4,36 +4,38 @@ public class SelectionSorter extends Sorter {
 	public SelectionSorter() {
 		type = "Selection";
 	}
-	
-	//  You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		//1. make a for loop to iterate through all but the last 
-		//   element of the array
-		for (int i = 0; i < array.length-2; i++) {
+		// 1. make a for loop to iterate through all but the last
+		// element of the array
+		for (int i = 0; i < array.length - 1; i++) {
 			int index = i;
-			for (int j = 0; j < array.length; j++) {
-				if(array[j] < index) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[index]) {
 					index = j;
-					int first = array[index];
-					int second = array[i];
-					array[i] = first;
-					array[index] = second;
-					display.updateDisplay();
+
 				}
+				display.updateDisplay();
 			}
+			int first = array[index];
+			int second = array[i];
+			array[i] = first;
+			array[index] = second;
+
 		}
-			//2. create an integer called index and set it equal to i
-			
-			//3. make another for loop that starts at i + 1 and
-			//   goes through the entire array
-			
-				//4. if the array element at j is less than the 
-				//   element at index, then set index equal to j
-				
-			//5. swap the array element at index with the array element at i
-			
-		
+
+		// 2. create an integer called index and set it equal to i
+
+		// 3. make another for loop that starts at i + 1 and
+		// goes through the entire array
+
+		// 4. if the array element at j is less than the
+		// element at index, then set index equal to j
+
+		// 5. swap the array element at index with the array element at i
+
 	}
 }
