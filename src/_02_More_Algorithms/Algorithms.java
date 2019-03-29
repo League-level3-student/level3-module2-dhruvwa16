0c55426b1b045ae2ef2e32aa551618cd4a649f6e
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-03-29 16:37:02 -0700

    Finished module 2

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package _02_More_Algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -57,11 +58,48 @@ public class Algorithms {
 		return false;
 		
 	}
-	public static void sortScores( List<Double> results) {
-		for (int i = 0; i < results.size(); i++) {
-			
+	public static Object sortDNA(List<String> unsortedSequences) {
+		// TODO Auto-generated method stub
+		ArrayList<String> unsorted2=new ArrayList<String>();
+		for (int i = 0; i < 28; i++) {
+			for (int j = 0; j < unsortedSequences.size(); j++) {
+				if(unsortedSequences.get(j).length() == i) {
+					unsorted2.add(unsortedSequences.get(j));
+				}
+			}
 		}
+		
+		unsortedSequences = unsorted2;
+		
+		return unsorted2;
+	}
+	
+	
+	public static List<String> sortWords(List<String> words) {
+		// TODO Auto-generated method stub
+		ArrayList<String> words2=new ArrayList<String>();
+		
+		for (int j = 0; j < words.size(); j++) {
+			int counter = 0;
+		for (int i = 0; i < words.size(); i++) {
+			if(words.get(j).compareTo(words.get(i)) > 0) {
+				counter++;
+			}
+		}
+		words2.add(counter, words.get(j));
+		
+		}
+		words = words2;
+		return words;
+	}
+	
+	
+
+		// TODO Auto-generated method stub
+		
 	}
 
 	
-}
+
+	
+
